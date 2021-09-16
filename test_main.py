923f39b85f26f93d786331a3d2092bf70ff9deac
conghaowong <conghao_wong@icloud.com>
2021-09-16 22:10:34 +0800

    update test method

--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@
 @Author: Conghao Wong
 @Date: 2021-09-16 20:00:49
 @LastEditors: Conghao Wong
-@LastEditTime: 2021-09-16 20:43:41
+@LastEditTime: 2021-09-16 21:51:59
 @Description: file content
 @Github: https://github.com/conghaowoooong
 @Copyright 2021 Conghao Wong, All Rights Reserved.
@@ -18,7 +18,7 @@ from main import train_or_test
 from modules import models as M
 
 
-def linear_pred():
+def train():
     fa = [sys.argv[0]] + ['--model', 'test', '--epochs', '5',
                           '--use_maps', '0', '--step', '4',
                           '--save_base_dir', './.test']
@@ -26,12 +26,21 @@ def linear_pred():
 
     train_or_test(args, force_args=fa)
 
+def evaluate():
+
+    p = './.test'
+    dir_name = os.listdir(p)[0]
+    fa = [sys.argv[0]] + ['--load', os.path.join(p, dir_name)]
+    args = M.prediction.PredictionArgs(fa)
+
+    train_or_test(args, force_args=fa)
 
 def test_pred():
     if os.path.exists(p := './.test'):
         shutil.rmtree(p)
 
-    linear_pred()
+    train()
+    evaluate()
 
 
 if __name__ == '__main__':
