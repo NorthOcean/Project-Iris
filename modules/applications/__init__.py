49e5d78fbe80a21be3de639b4a66b4d8b4c63cb6
conghaowong <conghao_wong@icloud.com>
2021-09-16 20:46:01 +0800

    add linear and test

--- modules/applications/__init__.py
+++ modules/applications/__init__.py
@@ -2,11 +2,11 @@
 @Author: Conghao Wong
 @Date: 2021-04-30 15:39:14
 @LastEditors: Conghao Wong
-@LastEditTime: 2021-07-30 11:02:21
+@LastEditTime: 2021-09-16 19:53:06
 @Description: file content
 @Github: https://github.com/conghaowoooong
 @Copyright 2021 Conghao Wong, All Rights Reserved.
 """
 
+from ._linearPrediction._linear import LinearLayer
 from ._transformer._transformer import Transformer
-# from ._transformer._utils import create_masks as create_transformer_masks
