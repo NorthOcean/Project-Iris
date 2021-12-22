9f628b0e63dbcb6f3434bdaf964c2f69b0053003
conghaowong <conghao_wong@icloud.com>
2021-12-22 20:17:03 +0800

    Silverballers.handlers
    - 添加handler模型`Burnwood`和`BurnwoodC`
      - `Burnwood`模型
        - 通过简化的TransformerEncoder和特征外积操作完成特征细化处理的模型
        - 使用FFT
        - 使用contextMap
        - 外积作用于轨迹特征和context特征的组合特征上
      - `BurnwoodC`模型
        - 基于`Burnwood`模型，精简参数
        - 轨迹特征维度降低为d/4
        - context特征维度降低为d/4
        - 在外积操作后添加一个池化层(2*2)

---
+++ modules/Silverballers/handlers/__init__.py
@@ -0,0 +1,12 @@
+"""
+@Author: Conghao Wong
+@Date: 2021-12-22 19:54:49
+@LastEditors: Conghao Wong
+@LastEditTime: 2021-12-22 19:54:49
+@Description: file content
+@Github: https://github.com/conghaowoooong
+@Copyright 2021 Conghao Wong, All Rights Reserved.
+"""
+
+from ._burnwood import Burnwood, BurnwoodModel
+from ._burnwoodC import BurnwoodC, BurnwoodCModel
